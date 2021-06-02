aba22f3994b6222c609bcb19f767d0a8b905f072
sim-ware <samir.gossain@gmail.com>
2021-06-02 22:33:38 +0100

    More comment and console log tidying - onto cumulating the units for the booleans

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,6 +1,6 @@
 export function stringify(date: Date, givenNow: Date): string {
   let now = givenNow ? givenNow : new Date()
-  // 
+
   // before finding the below, we need to know whether the number is rounded or not
   // if milliseconds === 0,                                       then we've rounded seconds
   // if seconds      === 0,      and the above,                   then we've rounded minutes
@@ -9,7 +9,6 @@ export function stringify(date: Date, givenNow: Date): string {
   // if day          === Monday, and the above,                   then we've rounded weeks 
   // if date         === 1,      and the above, apart from weeks, then we've rounded Months
   // if month        === 1,      and the above, apart from weeks, then we've rounded Years
-  // 
   const areSecondsRounded = date.getUTCMilliseconds() === 0
   const areMinutesRounded = date.getUTCSeconds()      === 0 // and the above
   const areHoursRounded   = date.getUTCMinutes()      === 0 // and the above
@@ -17,25 +16,24 @@ export function stringify(date: Date, givenNow: Date): string {
   const areWeeksRounded   = date.getUTCDay()          === 1 // and the above
   const areMonthsRounded  = date.getUTCDate()         === 1 // and the above, apart from weeks
   const areYearsRounded   = date.getUTCMonth()        === 0 // and the above, apart from weeks
+  console.log(`--------------date:${date.toLocaleDateString()}`)
+  console.log(`---------------now:${now.toLocaleDateString()}`)
+  console.log(`-areSecondsRounded:${areSecondsRounded}`)
+  console.log(`-areMinutesRounded:${areMinutesRounded}`)
+  console.log(`---areHoursRounded:${areHoursRounded}`)
+  console.log(`----areDaysRounded:${areDaysRounded}`)
+  console.log(`---areWeeksRounded:${areWeeksRounded}`)
+  console.log(`--areMonthsRounded:${areMonthsRounded}`)
+  console.log(`---areYearsRounded:${areYearsRounded}`)
 
-  console.log('-date:', date.toLocaleDateString())
-  console.log('--now:', now.toLocaleDateString())
-  console.log(`areSecondsRounded:${areSecondsRounded}`)
-  console.log(`areMinutesRounded:${areMinutesRounded}`)
-  console.log(`areHoursRounded:${areHoursRounded}`)
-  console.log(`areDaysRounded:${areDaysRounded}`)
-  console.log(`areWeeksRounded:${areWeeksRounded}`)
-  console.log(`areMonthsRounded:${areMonthsRounded}`)
-  console.log(`areYearsRounded:${areYearsRounded}`)
-  // 
   // const differenceInSeconds = (now.getTime() - date.getTime()) / 1000;
   // const yearsBetween = Math.floor(differenceInSeconds / 31536000);
   // const monthsBetween = Math.floor((differenceInSeconds % 31536000) / 2628000);
   // const daysBetween = Math.floor(((differenceInSeconds % 31536000) % 2628000) / 86400);
-  // console.log('differenceInSeconds:', differenceInSeconds)
   // console.log('yearsBetween:', yearsBetween)
   // console.log('monthsBetween:', monthsBetween)
   // console.log('daysBetween:', daysBetween)
+  // console.log('differenceInSeconds:', differenceInSeconds)
   // 
 
   return 'now-1y/y' // Fake News
