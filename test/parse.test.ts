cbc173034cf697e1c1eba48b8c9f2ebd0b07959b
sim-ware <samir.gossain@gmail.com>
2021-06-02 18:41:43 +0100

    further tidy parse utility integration scaffolding

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -3,13 +3,14 @@ import { parse } from "../src/parse";
 
 describe("__parse__", () => {
   it("should take a datestring and return a Date", () => {
-    // // // // // // // // // // // // // // // // // // // 
-    // expect(parse('now-1y/y')).toBe('now-1y/y');
-    // expect(parse('now/y')).toBe('now/y');
-    // expect(parse('now-1d')).toBe('now-1d');
-    // expect(parse('now+1d')).toBe('now+1d');
-    // expect(parse('now-4d-4h')).toBe('now-4d-4h');
-    // // // // // // // // // // // // // // // // // // // 
+    // // // // // // // // // // // // // // // // // // // // // 
+    // Given the current date and time is `2020-05-01T00:00:00`:
+    // now-1y/y  -> 2019-01-01T00:00:00 - now minus one year rounded to the nearest year
+    // now/y     -> 2020-01-01T00:00:00 - now rounded to the nearest year
+    // now-1d    -> 2020-04-30T00:00:00 - now minus 1 day
+    // now+1d    -> 2020-05-02T00:00:00 - now add 1 day
+    // now-4d-4h -> 2020-04-26T20:00:00 - now minus four days and four hours
+    // // // // // // // // // // // // // // // // // // // // // // // // //
     expect(true).toBe(true)
   });
 });
