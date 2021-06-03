71bc200e613f7db23b95ceddc4f6a97132e8101a
sim-ware <samir.gossain@gmail.com>
2021-06-03 09:45:32 +0100

    switches initial test to simpler use case, add commented notes for how to handle dates whether rounded or not

--- src/stringify.ts
+++ src/stringify.ts
@@ -6,6 +6,15 @@ export function stringify(date: Date, givenNow?: Date): string {
   const dateRound = checkForAndGetDateRoundValue(date, now)
   console.log(`dateRound:............:${dateRound}`)
 
+  // if rounded by years, get yearsBetween
+  // if rounded by months, get months and years between
+  // if rounded by weeks, get whole weeks or days between, months, and years between
+  // if rounded by days, get days, months, and years between
+  // if rounded by hours, get hours, days, months, and years between
+  // if rounded by minutes, get minutes, hours, days, months, and years between
+  // if rounded by seconds, get seconds, minutes, hours, days, months, and years between
+  // if not rounded at all - get all of the above?
+
   const differenceInSeconds = (now.getTime() - date.getTime()) / 1000;
   const yearsBetween = Math.floor(differenceInSeconds / 31536000);
   const monthsBetween = Math.floor((differenceInSeconds % 31536000) / 2628000);
@@ -15,5 +24,5 @@ export function stringify(date: Date, givenNow?: Date): string {
   console.log('monthsBetween:', monthsBetween)
   console.log('daysBetween:', daysBetween)
 
-  return 'now-1y/y' // Fake News
+  return 'now/y' // Fake News
 }
--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -4,11 +4,11 @@ import { stringify } from "../src/stringify";
 describe("__stringify__", () => {// Given the current date and time is `2020-05-01T00:00:00`:
   it("should take a Date and return a datestring", () => {
     // now-1y/y -> 2019-01-01T00:00:00 - now minus one year rounded to the nearest year
-    expect(
-      stringify(new Date('2019-01-01T00:00:00'), new Date('2020-05-01 00:00:0'))
-    ).toStrictEqual('now-1y/y')
     // expect(
-    //   stringify(new Date('2020-01-01T00:00:00'), new Date('2020-05-01 00:00:0'))
-    // ).toStrictEqual('now/y')
+    //   stringify(new Date('2019-01-01T00:00:00'), new Date('2020-05-01 00:00:0'))
+    // ).toStrictEqual('now-1y/y')
+    expect(
+      stringify(new Date('2020-01-01T00:00:00'), new Date('2020-05-01 00:00:0'))
+    ).toStrictEqual('now/y')
   });
 });
